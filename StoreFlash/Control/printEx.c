5d51fad823bef49c77621d5bea38045e410cc38a
NhanPA <annhandt09@gmail.com>
2020-08-06 22:38:43 +0700

    Update printEx.c

--- StoreFlash/Control/printEx.c
+++ StoreFlash/Control/printEx.c
@@ -6,7 +6,6 @@
 void vprint(const char *fmt, va_list argp)
 {
     char string[200];
-
     if(0 < vsprintf(string,fmt,argp)) // build string
     {
         HAL_UART_Transmit(&huart1, (uint8_t*)string, strlen(string), 100); // send message via UART HAL_UART_Transmit
@@ -15,7 +14,7 @@ void vprint(const char *fmt, va_list argp)
 void my_printf(const char *fmt, ...) // custom printf() function
 {
     va_list argp;
-    va_start(argp, fmt);
+    va_start(argp, fmt); //lấy các tham số sau tham số fmt
     vprint(fmt, argp);
     va_end(argp);
 }
